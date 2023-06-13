dd6c520e7428aafd91eae6d3c09c2413043b42f2
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-13 09:18:37 +0330

    Build Header initial structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,8 @@
-const Header = () => <>Header</>;
+const Header = () => (
+  <div className="todo-header">
+    <h1>Todos</h1>
+    <p>(Items will persist in the browser local storage)</p>
+  </div>
+);
 
 export default Header;
