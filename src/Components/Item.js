f7989cb095f178fbe456e61a8dba0b166fb22adb
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-13 09:40:19 +0330

    Build and style initial structure

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,3 +1,11 @@
-const Item = () => <div className="item-container">Item</div>;
+const Item = () => (
+  <div className="item-container">
+    <div>
+      <input type="checkbox" />
+      <span className="item-content">item content</span>
+    </div>
+    <img src="https://img.icons8.com/matisse/20/waste.png" alt="waste" />
+  </div>
+);
 
 export default Item;
