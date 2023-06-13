a0a73c32567263d654a219e37d0afe82cebd2f68
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-13 11:57:22 +0330

    Add item to local storage and reset it

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,8 +1,45 @@
-const Input = () => (
-  <div className="input-container">
-    <input placeholder="Add your task ..." />
-    <img src="https://img.icons8.com/ios-filled/30/plus.png" alt="plus" />
-  </div>
-);
+/*eslint-disable */
+import { useState } from "react";
+
+const Input = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  // Add button
+  const handleClick = () => {
+    const newItems = items;
+    newItems.push({
+      id: Math.floor(Math.random() * 1000),
+      content: inputValue,
+      checkStatus: true,
+    });
+    setItems(newItems);
+    // Update localstorage
+    localStorage.setItem("Items", JSON.stringify(items));
+    // Reset the input
+    setInputValue("");
+  };
+
+  return (
+    <div className="input-container">
+      <input
+        placeholder="Add your task ..."
+        value={inputValue}
+        onChange={(e) => {
+          handleChange(e);
+        }}
+      />
+      <img
+        src="https://img.icons8.com/ios-filled/30/plus.png"
+        alt="plus"
+        onClick={() => {
+          handleClick();
+        }}
+      />
+    </div>
+  );
+};
 
 export default Input;
